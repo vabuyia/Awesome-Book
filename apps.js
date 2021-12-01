698cdc7e1600e94d0df87a5e5669bca7c0d7c8af
Melanie Arellano <melaniesigrid@protonmail.com>
2021-12-01 10:52:15 -0600

    Correct minor variable errors

--- apps.js
+++ apps.js
@@ -10,38 +10,36 @@ class book {
       author: this.author,
     };
     
-    const listedBooks = JSON.parse(localStorage.getItem('bookList') || "[]");
+    const listedBooks = JSON.parse(localStorage.getItem('bookList') || '[]');
     listedBooks.push(book);
     localStorage.setItem('bookList', JSON.stringify(listedBooks));
-  };
+  }
   
-  deleteBook(index) {
+  deleteBook(id) {
     let listedBooks = JSON.parse(localStorage.getItem('bookList') || '[]');
     listedBooks = listedBooks.filter((book, index) => index !== parseInt(id, 10));
     localStorage.setItem('bookList', JSON.stringify(listedBooks));
   }
 }
-
-const title = document.querySelector('.title').value;
-const author = document.querySelector('.author').value;
-const addBook = document.querySelector('.add');
-const listShowContainer = document.querySelector('.listShow');
+const theTitle = document.querySelector('.title').value;
+const theAuthor = document.querySelector('.author').value;
+const addBook = document.querySelector('#add');
 
 const displayLibrary = () => {
-  const listedBooks = JSON.parse(localStorage.getItem('bookList') || "[]");
-  if (listedBooks.length == 0) {
+  const listedBooks = JSON.parse(localStorage.getItem('bookList') || '[]');
+  if (listedBooks.length === 0) {
     document.getElementsByClassName('listShow').innerHTML = `
     <div class="book">
       <span class="book-title">No books stored in library.</span>
     </div>`;
   } else {
     let codedBooks = '';
-    for (let i = 0; i <= bookList.length; i++) {
+    for (let i = 0; i <= listedBooks.length; i++) {
       codedBooks += `
       <div class="book">
-        <span class="book-title">${bookList[i].title}</span>
+        <span class="book-title">${listedBooks[i].title}</span>
         <br>
-        <span class="book-author">${bookList[i].author}</span>
+        <span class="book-author">${listedBooks[i].author}</span>
         <br>
         <button class="remove-button" id="remove-button" data-index="${i}">Remove</button>
         <hr>
@@ -51,10 +49,10 @@ const displayLibrary = () => {
     document.getElementsByClassName('list-show').innerHTML = codedBooks;
     if (listedBooks.length > 0) {
       const deleteButton = document.querySelectorAll('#remove-button');
-      deleteButton.forEach((deleteButton) => {
-        deleteButton.addEventListener('click', () => {
+      deleteButton.forEach((deleteBtt) => {
+        deleteBtt.addEventListener('click', () => {
           const aBook = new book('', '');
-          aBook.deleteBook(deleteButton.getAttribute('data-index'));
+          aBook.deleteBook(deleteBtt.getAttribute('data-index'));
           displayLibrary();
         });
       });
@@ -63,7 +61,7 @@ const displayLibrary = () => {
 };
 
 addBook.addEventListener('click', () => {
-  const newBook = new book(title.value, author.value);
+  const newBook = new book(theTitle.value, theAuthor.value);
   newBook.addBook();
   displayLibrary();
 });
