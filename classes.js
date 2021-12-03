63f1aac774e128f04204baab3bcb88530b653ff2
Melanie Arellano <melaniesigrid@protonmail.com>
2021-12-03 15:34:49 -0600

    Correct linter error

--- classes.js
+++ classes.js
@@ -30,12 +30,12 @@ class Booklet {
 
       const titleSpan = document.createElement('span');
       titleSpan.className = 'book-title';
-      titleSpan.textContent = `${bookObject.title}` + " by  ";
+      titleSpan.textContent = `${bookObject.title} by `;
       div.appendChild(titleSpan);
 
       const authorSpan = document.createElement('span');
       authorSpan.className = 'book-author';
-      authorSpan.textContent = " " + `${bookObject.author}`;
+      authorSpan.textContent = ` ${bookObject.author}`;
       div.appendChild(authorSpan);
 
       const removeButton = document.createElement('button');
@@ -112,7 +112,7 @@ contact.addEventListener('click', () => {
 window.onload = () => {
   document.getElementById('add').style.display = 'none';
   document.getElementById('contact').style.display = 'none';
-  generateBooks();
+  this.generateBooks();
 };
 
 const date = document.getElementById('date');
